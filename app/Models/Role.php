0046f1d64108c0115eb8c4b7477dd35ab17c2eca
thiennam01 <thiennam010405@gmail.com>
2026-08-11 08:58:21 +0700

    translate comments to English
    Updated comment for permissions method to English.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -19,7 +19,7 @@ class Role extends Model
     }
 
     /**
-     * Quan hệ N-N với bảng permissions thông qua bảng trung gian role_has_permissions
+     * Many-to-many relationship with the permissions table through the intermediate table role_has_permissions
      */
     public function permissions(): BelongsToMany
     {
